770817e95c13a03ced1dead562bb5e8d55ce984c
nwdepatie <nwdepatie@gmail.com>
2024-06-04 15:27:38 -0400

    Revamping state machine implementation

--- CM7/Core/Src/state_machine.c
+++ CM7/Core/Src/state_machine.c
@@ -5,9 +5,6 @@
 
 #define STATE_TRANS_QUEUE_SIZE 16
 
-/* Internal State of Vehicle */
-state_director_t proteus_state;
-
 /* State Transition Map */
 // TODO: fill out state transition map
 static const bool valid_trans_to_from[MAX_FUNC_STATES][MAX_FUNC_STATES] = {
@@ -24,64 +21,68 @@ static const bool valid_trans_to_from[MAX_FUNC_STATES][MAX_FUNC_STATES] = {
     {true, true, false, false, false, false, false, false, true, true},    /* FAULTED */
 };
 
-osThreadId_t sm_director_handle;
+void state_machine_init(state_machine_t *sm)
+{
+    sm->current_state = LV_BOOT;
+
+    sm->state_mutex = osMutexNew(&sm->state_mutex_attr);
+	assert(sm->state_mutex);
+
+    sm->state_trans_queue = osMessageQueueNew(STATE_TRANS_QUEUE_SIZE, sizeof(state_t), NULL);
+}
+
+int state_machine_queue_state(state_machine_t *sm, state_t new_state)
+{
+    if (!sm->state_trans_queue)
+        return 1;
+
+    return osMessageQueuePut(sm->state_trans_queue, &new_state, 0U, 0U);
+}
+
+state_t state_machine_get_state(state_machine_t *sm)
+{
+    state_t ret;
+    osStatus_t mut_stat = osMutexAcquire(sm->state_mutex, osWaitForever);
+	if (mut_stat) {
+		return -1;
+	}
+
+    ret = sm->current_state;
+
+    osMutexRelease(sm->state_mutex);
+
+    return ret;
+}
+
 const osThreadAttr_t sm_director_attributes = {
     .name = "State Machine Director",
     .stack_size = 128 * 4,
     .priority = (osPriority_t)osPriorityAboveNormal3,
 };
 
-static osMessageQueueId_t state_trans_queue;
-
-int queue_state(state_t new_state)
-{
-    if (!state_trans_queue)
-        return 1;
-
-    return osMessageQueuePut(state_trans_queue, &new_state, 0U, 0U);
-}
-
-state_t get_state()
-{
-    state_t ret;
-    osStatus_t mut_stat = osMutexAcquire(proteus_state.state_mutex, osWaitForever);
-	if (mut_stat) {
-		return -1;
-	}
-
-    ret = proteus_state.current_state;
-
-    osMutexRelease(proteus_state.state_mutex);
-
-    return ret;
-}
-
 void vStateMachineDirector(void *pv_params)
 {
-    proteus_state.current_state = LV_BOOT;
-
-    proteus_state.state_mutex = osMutexNew(&proteus_state.state_mutex_attr);
-	assert(proteus_state.state_mutex);
-
-    state_trans_queue = osMessageQueueNew(STATE_TRANS_QUEUE_SIZE, sizeof(state_t), NULL);
+    state_machine_t *sm = (state_machine_t *)pv_params;
+	assert(sm);
 
     state_t new_state;
 
     for (;;)
     {
-        if (osOK != osMessageQueueGet(state_trans_queue, &new_state, NULL, 50))
+        if (osOK != osMessageQueueGet(sm->state_trans_queue, &new_state, NULL, 50))
         {
             // TODO queue fault, low criticality
             continue;
         }
 
-        //if (!valid_trans_to_from[new_state][cerberus_state])
-        //{
-        //    // TODO queue fault, low criticality
-        //    continue;
-        //}
-//
-        //// transition state via LUT ?
-        //cerberus_state = new_state;
+        if (!valid_trans_to_from[new_state][sm->current_state])
+        {
+            // TODO queue fault, low criticality
+            continue;
+        }
+
+        printf("Transitioning state!\r\n");
+        /* transition state via LUT */
+        sm->current_state = new_state;
     }
 }
