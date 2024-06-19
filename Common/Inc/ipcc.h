5c95d12c73df5f0c3877209dcb4247ce4cea29ce
Jack Rubacha <rubacha.jack03@gmail.com>
2024-06-18 20:21:49 -0400

    further fix formatting

--- Common/Inc/ipcc.h
+++ Common/Inc/ipcc.h
@@ -6,32 +6,33 @@
 #include "fdcan.h"
 
 typedef struct {
-    enum {
-        IPCC_CAN_MSG,
-        IPCC_UART_MSG,
-        IPCC_STATE_TRANSITION,
-        IPCC_FAULT_ALERT
-    } id;
-    union {
-        uint32_t can;
-        char serial[PRINTF_BUFFER_LEN];
-        state_t state;
-        fault_data_t fault;
-    } msg;
+	enum {
+		IPCC_CAN_MSG,
+		IPCC_UART_MSG,
+		IPCC_STATE_TRANSITION,
+		IPCC_FAULT_ALERT
+	} id;
+	union {
+		uint32_t can;
+		char serial[PRINTF_BUFFER_LEN];
+		state_t state;
+		fault_data_t fault;
+	} msg;
 } ipcc_msg_t;
 
 typedef struct {
-    DMA_HandleTypeDef *tx_dma;
-    volatile ipcc_msg_t *rx_mem;
-    volatile ipcc_msg_t *tx_mem;
-    int tx_sem_id;  /* For notification of other core */
-    int rx_sem_id;  /* For acknowledgement of transmission */
+	DMA_HandleTypeDef *tx_dma;
+	volatile ipcc_msg_t *rx_mem;
+	volatile ipcc_msg_t *tx_mem;
+	int tx_sem_id; /* For notification of other core */
+	int rx_sem_id; /* For acknowledgement of transmission */
 } ipcc_t;
 
 /**
  * Initializes the Inter Processor Communication Channel
 */
-void ipcc_init(ipcc_t *ipcc, DMA_HandleTypeDef *tx_dma, int tx_hsem_id, int rx_hsem_id);
+void ipcc_init(ipcc_t *ipcc, DMA_HandleTypeDef *tx_dma, int tx_hsem_id,
+	       int rx_hsem_id);
 
 /**
  * Initiates a transfer of data to the other processor
--- Common/Inc/proteus_config.h
+++ Common/Inc/proteus_config.h
@@ -5,41 +5,29 @@
 
 // TODO: add actual list of states
 typedef enum {
-    LV_BOOT,
-    CHARGE_BOOT_CAP,
-    OFFSET_CALIB,
-    CLEAR,
-    START,      /* Open Loop Startup */
-    START_RUN,
-    RUN,        /* Closed Loop Control */
-    STOP_IDLE,
-    STOP_NOW,
-    FAULTED,
-    MAX_FUNC_STATES
+	LV_BOOT,
+	CHARGE_BOOT_CAP,
+	OFFSET_CALIB,
+	CLEAR,
+	START, /* Open Loop Startup */
+	START_RUN,
+	RUN, /* Closed Loop Control */
+	STOP_IDLE,
+	STOP_NOW,
+	FAULTED,
+	MAX_FUNC_STATES
 } state_t;
 
 /* Faulting config */
-typedef enum
-{
-    DEFCON1 = 1,
-    DEFCON2,
-    DEFCON3,
-    DEFCON4,
-    DEFCON5
-} fault_sev_t;
+typedef enum { DEFCON1 = 1, DEFCON2, DEFCON3, DEFCON4, DEFCON5 } fault_sev_t;
 
 // TODO: add actual fault codes
-typedef enum
-{
-    FAULTS_CLEAR = 0x0,
-    MAX_FAULTS
-} fault_code_t;
+typedef enum { FAULTS_CLEAR = 0x0, MAX_FAULTS } fault_code_t;
 
-typedef struct
-{
-    fault_code_t id;
-    fault_sev_t severity;
-    char diag[PRINTF_BUFFER_LEN];
+typedef struct {
+	fault_code_t id;
+	fault_sev_t severity;
+	char diag[PRINTF_BUFFER_LEN];
 } fault_data_t;
 
 #endif /* PROTEUS_CONFIG_H */
